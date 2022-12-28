b65e8279c801e89b3c6324a06c0958eff9ed3352
Lior <levydlior@gmail.com>
2022-12-27 19:01:16 -0700

    add gender to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_24_180508) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_28_020006) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -26,6 +26,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_24_180508) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.string "gender"
   end
 
 end
