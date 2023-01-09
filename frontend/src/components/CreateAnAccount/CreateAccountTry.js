94b20029f29d22b8118c68c32e2ea9384dc16585
Lior <levydlior@gmail.com>
2023-01-09 14:04:42 -0700

    add month and day validation

--- frontend/src/components/CreateAnAccount/CreateAccountTry.js
+++ frontend/src/components/CreateAnAccount/CreateAccountTry.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { useFormik, Field } from "formik";
+import { useFormik, Field, FormikProvider } from "formik";
 import { basicSchema } from "../../schemas";
 
 function CreateAccountTry() {
@@ -9,86 +9,97 @@ function CreateAccountTry() {
       password: "",
       email: "",
       year: "",
-      month: "",
+      month: "12",
       day: "",
       gender: "",
     },
     validationSchema: basicSchema,
   });
-  console.log(formik.month);
+  console.log(formik.values.month);
   return (
     <div>
-      <form>
-        <label htmlFor="username"> Username </label>
-        <input
-          value={formik.values.username}
-          onChange={formik.handleChange}
-          name="username"
-          id="username"
-          type="text"
-          onBlur={formik.handleBlur}
-        />
-        <p>{formik.errors.username}</p>
-        <label htmlFor="password"> password </label>
-        <input
-          value={formik.values.password}
-          onChange={formik.handleChange}
-          name="password"
-          id="password"
-          type="password"
-          onBlur={formik.handleBlur}
-        />
-        <p>{formik.errors.password}</p>
-        <label htmlFor="email"> email </label>
-        <input
-          value={formik.values.email}
-          onChange={formik.handleChange}
-          name="email"
-          id="email"
-          type="email"
-          onBlur={formik.handleBlur}
-        />
-        <label htmlFor="month"> month</label>
-        <select 
-        value={formik.values.month} 
-        type="select" 
-        onChange={formik.handleChange}
-        name="month"
-        id="month"
-        >
-          <option value="1" label="January" />
-          <option value="2">February</option>
-          <option value="3">March</option>
-          <option value="4">April</option>
-          <option value="5">May</option>
-          <option value="6">June</option>
-          <option value="7">July</option>
-          <option value="8">August</option>
-          <option value="9">September</option>
-          <option value="10">October</option>
-          <option value="11">November</option>
-          <option value="12">December</option>
-        </select>
-        <label htmlFor="day"> Day </label>
-        <input
-          value={formik.values.day}
-          onChange={formik.handleChange}
-          name="day"
-          id="day"
-          type="date"
-          onBlur={formik.handleBlur}
-        />
-        <label htmlFor="gender"> gender </label>
-        <input
-          value={formik.values.gender}
-          onChange={formik.handleChange}
-          name="gender"
-          id="gender"
-          type="text"
-          onBlur={formik.handleBlur}
-        />
-        <button type="submit">Submit</button>
-      </form>
+      <FormikProvider value={formik}>
+        <form>
+          <label htmlFor="email"> What's your email?</label>
+          <input
+            value={formik.values.email}
+            onChange={formik.handleChange}
+            name="email"
+            id="email"
+            type="email"
+            onBlur={formik.handleBlur}
+          />
+          <p>{formik.errors.email}</p>
+          <label htmlFor="username"> Username </label>
+          <input
+            value={formik.values.username}
+            onChange={formik.handleChange}
+            name="username"
+            id="username"
+            type="text"
+            onBlur={formik.handleBlur}
+          />
+          <p>{formik.errors.username}</p>
+          <label htmlFor="password"> password </label>
+          <input
+            value={formik.values.password}
+            onChange={formik.handleChange}
+            name="password"
+            id="password"
+            type="password"
+            onBlur={formik.handleBlur}
+          />
+          <p>{formik.errors.password}</p>
+
+          <label htmlFor="date of birth">  What's your date of birth? </label>
+          <Field
+            name="month"
+            as="select"
+            value={formik.values.month || ""}
+            onChange={formik.handleChange}
+            id="month"
+          >
+            <option value="" disabled>
+              Month
+            </option>
+            <option value="1">January</option>
+            <option value="2">February</option>
+            <option value="3">March</option>
+            <option value="4">April</option>
+            <option value="5">May</option>
+            <option value="6">June</option>
+            <option value="7">July</option>
+            <option value="8">August</option>
+            <option value="9">September</option>
+            <option value="10">October</option>
+            <option value="11">November</option>
+            <option value="12">December</option>
+          </Field>
+          <label htmlFor="day"> Day </label>
+          <input
+            value={formik.values.day}
+            onChange={formik.handleChange}
+            name="day"
+            id="day"
+            type="text"
+            onBlur={formik.handleBlur}
+            maxlength={2}
+          />
+           <p>{formik.errors.day}</p>
+
+
+          <label htmlFor="gender"> gender </label>
+          <input
+            value={formik.values.gender}
+            onChange={formik.handleChange}
+            name="gender"
+            id="gender"
+            type="text"
+            onBlur={formik.handleBlur}
+          />
+          <button type="submit">Submit</button>
+        </form>
+      </FormikProvider>
     </div>
   );
 }
--- frontend/src/schemas/index.js
+++ frontend/src/schemas/index.js
@@ -1,7 +1,6 @@
 import * as yup from "yup";
 
 const passwordRules = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{5,}$/;
-const newDate = new Date()
 
 export const basicSchema = yup.object().shape({
     username: yup.string().required("Required"),
@@ -9,6 +8,6 @@ export const basicSchema = yup.object().shape({
     password: yup.string().min(5).matches(passwordRules, {message: "Please create a strong password"}).required("Required"),
     year: yup.number(),
       month: "",
-      day: "",
+      day: yup.number().required("Required").typeError("please enter a valid day").max(31, "please enter a valid day")
 
 })
